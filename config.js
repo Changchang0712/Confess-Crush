f50b798a9359b29bbc26c6b15ea8658845c2f095
Changchang0712 <102730893+Changchang0712@users.noreply.github.com>
2022-03-31 21:44:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi đến một cô gái siêu khó tánh",
+    titleWeb: "Xin chào cô gái khó tánh đáng êu",
     introTitle: 'Chôm êu',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim... lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Chị đồng ý làm người đặc biệt nhất của em suốt đời nha 🥰',
-    desc: 'BE MY VALENTINE ',
-    btnYes: 'Đồng ý <33',
+    btnIntro: '^^Nhấn dô đây nha^^',
+    title: 'Đồng ý làm người đặc biệt nhất của e suốt đời nha 🥰',
+    desc: 'Quyền lợi sẽ được bao nuôi suốt đời',
+    btnYes: 'Đồng ý nha <33',
     btnNo: 'Không nha :3',
-    messLink: 'https://www.facebook.com/changchan0407' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    question: 'Tuy bây giờ em chưa có điều kiện như bao người khác nhưng vẫn mong sẽ có được cơ hội từ chị như bao người khác <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: '',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Hôm nào hẹn hò nhá',
+    btnAccept: 'oce <3',
+    messLink: 'https://www.facebook.com/changchan0407' ' 
 }
