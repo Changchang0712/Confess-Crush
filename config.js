a43a061e3a8bf108e0ed77fdcd9fa0907ac52542
Changchang0712 <102730893+Changchang0712@users.noreply.github.com>
2022-03-31 22:08:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "GỬI MỘT CÔ GÁI KHÓ TÍNH ĐÁNG ÊU",
+    introTitle: 'CHÔM ÊU',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Lạc vào tim AI lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: '^^NHẤN VÔ ĐÂY NHA^^',
+    title: 'Chị đồng ý làm nhười đặc biết nhất của em suốt đời nha 🥰',
+    desc: 'Quyền lợi được bao nuôi suốt đời nha ',
+    btnYes: 'ĐỒNG Ý <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'EM CHẢ DÁM HỨA SẼ CHO 2 MỌI THỨ. CÁI EM CÓ THỂ LÀM LÀ CHO 2 NHỮNG GÌ EM CÓ TỐT NHẤT <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'ÊU ÊU <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'HÔM NÀO ĐI CAFE NHA NGƯỜI ĐẸP.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/changchan0407' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
